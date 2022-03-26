d5209a9bf774317af266b52e3f16b1c41c7eef2d
amorcar <alejandro.morales.carreno@gmail.com>
2022-03-26 20:03:07 +0000

    fix docker-compose file

--- client/post_task.py
+++ client/post_task.py
@@ -13,44 +13,50 @@ from company_descriptions import (
     pornhub,
 )
 
-api_URL = "http://127.0.0.1:8000/api/task"
+api_URL_container = "http://127.0.0.1:8004/api/task"
+api_URL_local = "http://127.0.0.1:8000/api/task"
+
 api_HEADERS = {
     'Content-type': 'application/json',
     'accept': 'application/json'
 }
 
-def post_task(text:str) -> Optional[str]:
+def post_task(text:str, url:str) -> Optional[str]:
     data = {'text': text}
-    r = requests.post(api_URL, headers=api_HEADERS, json=data)
+    r = requests.post(url, headers=api_HEADERS, json=data)
     return r.json().get('info', {}).get('id', None)
 
-def get_task_status(id:int) -> Tuple[Optional[str]]:
-    r = requests.get(api_URL+f'/{id}', headers=api_HEADERS)
+def get_task_status(id:int, url:str) -> Tuple[Optional[str]]:
+    r = requests.get(url+f'/{id}', headers=api_HEADERS)
     status = r.json().get('task_status', {}).get('status', None)
     result = r.json().get('result', {}).get('value', None)
     return (status, result)
 
 def predict_company(
     name: str,
-    description: str
+    description: str,
+    url:str,
 ):
-    id = post_task(description)
+    id = post_task(description, url)
     if id is None:
         print('Request failed')
         return
     print(f'Predicting category for {name}...')
     status = ''
     while status != 'SUCCESS':
-        status, result = get_task_status(id)
+        status, result = get_task_status(id, url)
         sleep(0.5)
     print(f'{name} category: {result}')
 
 if __name__ == '__main__':
-    predict_company('Booking', booking)
-    predict_company('Amazon Prime', amazon_prime)
-    predict_company('Amazon Prime Video', amazon_prime_video)
-    predict_company('CNN', cnn)
-    predict_company('FBI', fbi)
-    predict_company("L'equipe", lequipe)
-    predict_company('Pornhub', pornhub)
+    url = api_URL_container
+    # url = api_URL_local
+
+    predict_company('Booking', booking, url)
+    predict_company('Amazon Prime', amazon_prime, url)
+    predict_company('Amazon Prime Video', amazon_prime_video, url)
+    predict_company('CNN', cnn, url)
+    predict_company('FBI', fbi, url)
+    predict_company("L'equipe", lequipe, url)
+    predict_company('Pornhub', pornhub, url)
 
