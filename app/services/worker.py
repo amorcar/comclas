f1d006cb34ea34d967529d79c86e8d4e4201a57b
amorcar <alejandro.morales.carreno@gmail.com>
2022-03-26 11:05:00 +0000

    update docker-compose file

--- app/services/worker.py
+++ app/services/worker.py
@@ -6,8 +6,8 @@ from celery import Celery
 
 celery = Celery(
     __name__,
-    # broker=os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379"),
-    # backend=os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+    broker=os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379"),
+    backend=os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
 )
 
 
